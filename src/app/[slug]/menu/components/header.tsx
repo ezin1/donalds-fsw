e934dc16c631743e1fe48e29b032ae52e5956a2b
ezin1 <eziof.dev@gmail.com>
2025-02-26 17:02:10 -0300

    feat: implement restaurant header component and enhance menu page with restaurant data retrieval

---
+++ src/app/[slug]/menu/components/header.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { Restaurant } from "@prisma/client";
+import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+
+interface RestaurantHeaderProps {
+  restaurant: Pick<Restaurant, "coverImageUrl" | "name">;
+}
+
+const RestaurantHeader = ({ restaurant }: RestaurantHeaderProps) => {
+  const router = useRouter();
+
+  return (
+    <div className="relative h-[250px] w-full">
+      <Button
+        className="absolute left-4 top-4 z-50 rounded-full"
+        variant="secondary"
+        size="icon"
+        onClick={() => router.back()}
+      >
+        <ChevronLeftIcon />
+      </Button>
+      <Image
+        src={restaurant.coverImageUrl}
+        fill
+        alt={restaurant.name}
+        className="object-cover"
+      />
+      <Button
+        className="absolute right-4 top-4 z-50 rounded-full"
+        variant="secondary"
+        size="icon"
+      >
+        <ScrollTextIcon />
+      </Button>
+    </div>
+  );
+};
+
+export default RestaurantHeader;
--- src/app/[slug]/menu/page.tsx
+++ src/app/[slug]/menu/page.tsx
@@ -1,26 +1,38 @@
 import { notFound } from "next/navigation";
 
+import { getRestaurantBySlug } from "@/_data/restaurant/get-restaurant-by-slug";
+
+import RestaurantHeader from "./components/header";
+
 interface RestaurantMenuPageProps {
   params: Promise<{ slug: string }>;
   searchParams: Promise<{ consumptionMethod: string }>;
 }
 
-const RestaurantMenuPage = async ({ params, searchParams }: RestaurantMenuPageProps) => {
+const RestaurantMenuPage = async ({
+  params,
+  searchParams,
+}: RestaurantMenuPageProps) => {
   const { slug } = await params;
-  const { consumptionMethod} = await searchParams;
+  const { consumptionMethod } = await searchParams;
 
   const isConsumptionMethod = (consumptionMethod: string) => {
     return ["DINE_IN", "TAKEAWAY"].includes(consumptionMethod.toUpperCase());
-  }
+  };
 
   if (!isConsumptionMethod(consumptionMethod)) {
     return notFound();
   }
+
+  const restaurant = await getRestaurantBySlug({ slug });
+
+  if (!restaurant) {
+    return notFound();
+  }
   return (
-    <h1>
-      Restaurant Menu Page
-      {slug}
-    </h1>
+    <div>
+      <RestaurantHeader restaurant={restaurant} />
+    </div>
   );
 };
 
