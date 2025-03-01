909c8d112d4aa608d0a8cc202aa368b23d7302ce
ezin1 <eziof.dev@gmail.com>
2025-03-01 11:29:19 -0300

    feat: add quantity adjustment functionality and format currency display in product details

--- src/app/[slug]/menu/[productId]/components/product-details.tsx
+++ src/app/[slug]/menu/[productId]/components/product-details.tsx
@@ -1,5 +1,11 @@
+"use client"
 import { Product, Restaurant } from "@prisma/client";
+import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
 import Image from "next/image";
+import { useState } from "react";
+
+import { Button } from "@/components/ui/button";
+import { FormatCurrency } from "@/helpers/format-currency";
 
 interface ProductDetailsProps {
   restaurant: Pick<Restaurant, "name" | "avatarImageUrl">
@@ -7,6 +13,16 @@ interface ProductDetailsProps {
 }
 
 const ProductDetails = ({restaurant, product}: ProductDetailsProps) => {
+  const [quantity, setQuantity] = useState<number>(1);
+
+  const handleDrecreaseQuantity = () => {
+    setQuantity((prev) => prev - 1);
+  }
+
+  const handleIncreaseQuantity = () => {
+    setQuantity((prev) => prev + 1);
+  }
+
   return ( 
   <div className="relative z-50 rounded-t-3xl p-5 mt-[-1.5rem]">
     <div className="flex items-center gap-1.5">
@@ -19,9 +35,27 @@ const ProductDetails = ({restaurant, product}: ProductDetailsProps) => {
       />
       <p className="gap-1 text-xs text-muted-foreground">{restaurant.name}</p>
     </div>
-    <h2 className="text-xl font-semibold">{product.name}</h2>
+    <h2 className="mt-1 text-xl font-semibold">{product.name}</h2>
+    
+    <div className="flex items-center justify-between">
+      <h3 className="text-xl font-semibold">{FormatCurrency(product.price)}</h3>   
+      <div className="flex items-center gap-3 text-center">
+          <Button variant="outline" className="w-8 h-8 rounded-xl" onClick={handleDrecreaseQuantity} disabled={quantity === 1}>
+            <ChevronLeftIcon />
+          </Button>
+          <p className="w-4">{quantity}</p>
+          <Button variant="destructive" className="w-8 h-8 rounded-xl" onClick={handleIncreaseQuantity}>
+            <ChevronRightIcon />
+          </Button>
+      </div>  
+    </div>
+
+    <div className="mt-6 space-y-3">
+      <h4 className="font-semibold">Sobre</h4>
+      <p className="text-sm text-muted-foreground">{product.description}</p>
+    </div>
   </div> 
   );
 }
- 
+
 export default ProductDetails;
