6bfac45bd7bc5ce1308528f8354cb5a7d340427e
ezin1 <eziof.dev@gmail.com>
2025-03-06 10:29:14 -0300

    feat: add decrease product quantity functionality to cart context and component

--- src/app/[slug]/menu/contexts/cart.tsx
+++ src/app/[slug]/menu/contexts/cart.tsx
@@ -12,6 +12,7 @@ export interface ICartContext {
   products: CartProduct[];
   toggleCart: () => void;
   addProduct: (product: CartProduct) => void;
+  decreaseProductQuantity: (productId: string) => void;
 }
 
 export const CartContext = createContext<ICartContext>({
@@ -19,6 +20,7 @@ export const CartContext = createContext<ICartContext>({
   products: [],
   toggleCart: () => {},
   addProduct: () => {},
+  decreaseProductQuantity: () => {}
 });
 
 export const CartProvider = ({ children }: { children: ReactNode }) => {
@@ -54,8 +56,23 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
     
   };
 
+  const decreaseProductQuantity = (productId: string) => {
+    setProducts(prevProducts => {
+      return prevProducts.map(prevProduct => {
+        if (prevProduct.id === productId) {
+          return {
+            ...prevProduct,
+            quantity: prevProduct.quantity - 1
+          }
+        }
+
+        return prevProduct;
+      })
+    })
+  }
+
   return (
-    <CartContext.Provider value={{ isOpen, products, toggleCart, addProduct }}>
+    <CartContext.Provider value={{ isOpen, products, toggleCart, addProduct, decreaseProductQuantity }}>
       {children}
     </CartContext.Provider>
   );
